39945a3d4b632d1e0dcbd0d18df1628196d62b86
purav juneja <puravjuneja@gmail.com>
2022-07-08 10:44:26 +0530

    Banner added

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Fragment } from 'react';
 import GetInvolved from './components/getInvolved';
 import Footer from './components/footer';
 import Counter from './components/counter';
+import Fundraising from './components/fundraising';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <br/>
       <br/>
       <Counter/>
+      <Fundraising/>
       <Footer/>
     </Fragment>
   );
---
+++ src/components/fundraising.js
@@ -0,0 +1,28 @@
+import { Fragment } from "react";
+import './fundraising.css';
+
+const Fundraising = () =>{
+    return(
+        <div className = "fundRaising">
+            <div className="fundLeft">
+                <h5>Call To Action</h5>
+                <h1>Fundraising For The People And Causes You Care About</h1>
+                <p>Our motto is to strengthen the roots of society by uplifting the lives of underprivileged children and empowering women, enabling them to stand on their own feet and contribute to the community.</p>
+                <div className="playButton">
+                <i class="fa-solid fa-circle-play"></i>
+                    <p>PLAY DOCUMENTRY</p>
+                </div>
+            </div>
+            <div className="fundRight">
+                <h5>Call To Action</h5>
+                <h1>Fundraising For The People And Causes You Care About</h1>
+                <p>Our motto is to strengthen the roots of society by uplifting the lives of underprivileged children and empowering women, enabling them to stand on their own feet and contribute to the community.</p>
+                <div className="playButton">
+                    <button></button>
+                    <p>PLAY DOCUMENTRY</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default Fundraising;
