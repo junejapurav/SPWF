24ec15772b554209c6608abc00ca03299cb82934
Pranjal <pranjalsingh531@gmail.com>
2022-07-18 18:56:20 +0530

    updatedimg

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
 import {
-  BrowserRouter,
   Routes,
   Route,
 } from "react-router-dom";
-
-import Header from './components/header'
 import './App.css';
-import { Fragment } from 'react';
-import GetInvolved from './components/getInvolved';
-import Footer from './components/footer';
-import Counter from './components/counter';
-import Slider from './components/slider';
-import RecentEvents from './components/RecentEvents';
-import Fundraising from './components/fundraising';
 import HomePage from "./MainComponents/HomePage";
 import AboutUsPage from './MainComponents/AboutUsPage'
 import ContactUsPage from './MainComponents/ContactUsPage'
-
 
 function App() {
   
@@ -24,19 +13,13 @@ function App() {
     <div>
       <main>
     
-    <Routes>
-      
-      <Route path = '/' element={<HomePage/>}>
+        <Routes>
+          <Route path = '/' element={<HomePage/>}></Route>
+          <Route path='/aboutUs' element={<AboutUsPage/>} ></Route>
+          <Route path='/contactUs' element={<ContactUsPage/>} ></Route>
+        </Routes>
         
-      </Route>
-      {/* <Route path = '/aboutUs'>
-        <Header/>
-        <Footer/>
-      </Route> */}
-      <Route path='/aboutUs' element={<AboutUsPage/>} ></Route>
-      <Route path='/contactUs' element={<ContactUsPage/>} ></Route>
-    </Routes>
-    </main>
+      </main>
     </div>
   );
 }
