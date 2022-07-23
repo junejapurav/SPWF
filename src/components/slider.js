8ef6a1ef5935c01b9beaf000e8c846e2fa807d4c
purav juneja <puravjuneja@gmail.com>
2022-07-23 22:03:23 +0530

    addedShiksarth

--- src/components/slider.js
+++ src/components/slider.js
@@ -1,9 +1,9 @@
 import Carousel from 'react-bootstrap/Carousel';
 import './slider.css';
-import pic1 from '../images/Picture1.png';
-import pic3 from '../images/Picture2.jpg';
-import pic2 from '../img/pic1.jpg';
-import pic4 from '../images/Picture4.png';
+import pic1 from '../img/Picture12.jpg';
+import pic3 from '../img/Picture14.jpg';
+import pic2 from '../img/shiksharth.jpg';
+import pic4 from '../img/Picture13.jpg';
 
 function Slider() {
   return (
@@ -11,7 +11,7 @@ function Slider() {
       <Carousel.Item interval={1000}>
         <img
           className="d-block w-100 mypics"
-          src={pic1}
+          src={pic2}
           alt="First slide"
         />
         <Carousel.Caption>
@@ -22,7 +22,7 @@ function Slider() {
       <Carousel.Item interval={500}>
         <img
           className="d-block w-100 mypics"
-          src={pic2}
+          src={pic1}
           alt="Second slide"
         />
         <Carousel.Caption>
@@ -34,6 +34,18 @@ function Slider() {
         <img
           className="d-block w-100 mypics"
           src={pic4}
+          alt="Third slide"
+        />
+        <Carousel.Caption>
+          <h1 className='let'>Let's Build A Better World Together</h1>
+          <p>
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100 mypics"
+          src={pic3}
           alt="Third slide"
         />
         <Carousel.Caption>
