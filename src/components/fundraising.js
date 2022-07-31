60f5b22912c6ccdd55c0785d81e71c0de49bdb6f
Pranjal <pranjalsingh531@gmail.com>
2022-07-31 16:14:57 +0530

    updated

--- src/components/fundraising.js
+++ src/components/fundraising.js
@@ -10,7 +10,7 @@ const Fundraising = () =>{
                 <h1>FROM DOING CHORES TO SHINING IN SCHOOL: RUPALI’S STORY </h1>
                 <p>Rupali belongs to an impoverished family residing in Uttar Pradesh. Due to work, her whole family had to migrate to Delhi.The living, her father earned used to get exhausted in his alcohol consumption, eventually leaving the financial needs of the house unfulfilled. Rupali’s mother suffered from illness making her unable to work. Thus, Rupali had to contribute to the earnings of the family. After completing chores at home, she used to work as a domestic help at others’ houses. Rupali had never gone to school. However, having a desire to study, she joined our project “Shiksharth”. She continued to learn with passion and determination despite several adversities. </p>
                 <div className="playButton">
-                <i class="fa-solid fa-circle-play"></i>
+                <a  style={{color:"white"}} href="https://youtu.be/dB33mlhTm2s" ><i class="fa-solid fa-circle-play"></i></a>
                     <p>PLAY DOCUMENTRY</p>
                 </div>
             </div>
