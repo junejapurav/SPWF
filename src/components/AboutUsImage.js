f2f2ce65f320c3d3d5168465c654429b2e62de5f
Pranjal <pranjalsingh531@gmail.com>
2022-07-31 17:20:51 +0530

    added doatioon img

--- src/components/AboutUsImage.js
+++ src/components/AboutUsImage.js
@@ -3,7 +3,7 @@ import './AboutUsImage.css'
 function AboutUsImage(props){
 
     return(
-        <div className="AboutUsImage" style={{backgroundImage:`url(${props.image})`,backgroundSize: 'cover',backgroundRepeat:'no-repeat',textAlign:'center',height:'300px'}}>
+        <div className="AboutUsImage" style={{backgroundImage:`url(${props.image})`,backgroundSize: 'cover',backgroundRepeat:'no-repeat',textAlign:'center',height:'325px'}}>
             <h1 style={{color:'white',paddingTop:'100px',}}>{props.heading}</h1>
         </div>
     )
