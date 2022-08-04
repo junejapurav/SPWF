4e133ed5e682caf702e0931799854c956b4f0133
Pranjal <pranjalsingh531@gmail.com>
2022-08-04 14:31:52 +0530

    completed

--- src/components/Admin/admin.js
+++ src/components/Admin/admin.js
@@ -2,34 +2,55 @@ import { useState } from "react";
 import ShowContactUsData from './ShowContactUsData'
 import styles from './admin.module.css'
 import { useNavigate } from "react-router-dom";
+import ShowContactUsDataMain from "./ShowContactUsDataMain";
+import ShowVolunteerDataMain from "./ShowVolunteerDataMain";
+import ShowDonationDataMain from "./ShowDonationDataMain";
 
 const Admin = (props)=>{
-      
-  let navigate = useNavigate(); 
-  const routeChange = (path) =>{
-    navigate(path);
-  }
-
+    
+    const [contactData, showContactData] = useState(false);
+    const [volunteerData, showVolunteerData] = useState(false);
+    const [donationData, showDonationData] = useState(false);
+    const [checkLogin,changeChecker] = useState(props.checkLogin)
 
     function showContactUsDataHandler(){
-        routeChange('contact-us-data');
+        showContactData(true);
+        changeChecker(false);
+    }
+    function showDonationDataHandler(){
+        showDonationData(true);
+        changeChecker(false);
     }
     function showVolunteerDataHandler(){
-        routeChange('volunteer-data');
+        showVolunteerData(true);
+        changeChecker(false);
     }
-    function showDonationData(){
-        routeChange('donation-data');
+    function backHandler(){
+        changeChecker(true);
+        showContactData(false);
+        showVolunteerData(false);
+        showDonationData(false);
     }
- 
+
+
+
     return(
        
         <div>
-            {props.checkLogin && <div className={styles.main}>
+           {!checkLogin && <div style={{textDecoration:"underline",cursor:"pointer",maxWidth:"10%"} } onClick={backHandler}>
+                <i className="fa-solid fa-arrow-left" style={{color:"orange"}}></i>
+                Back
+           </div>}
+            {contactData && <ShowContactUsDataMain/>}
+            {volunteerData && <ShowVolunteerDataMain/>}
+            {donationData && <ShowDonationDataMain/>}
+            
+            {checkLogin && <div className={styles.main}>
                 <h1>Welcome to the Admin Panel</h1>
                 <h3>Which data do you want to access?</h3>
                 <p onClick={showContactUsDataHandler}>Show contact us data</p>
                 <p onClick={showVolunteerDataHandler}>Show Volunteer Applicants Data</p>
-                <p onClick={showDonationData}>Show Donation Data</p>
+                <p onClick={showDonationDataHandler}>Show Donation Data</p>
 
             </div>}
             {!props.checkLogin && <p>Unauthorized Access</p>}
