1bc0b98a7b0f895dc5e5a28a82c7f40b85248258
purav juneja <puravjuneja@gmail.com>
2022-08-05 12:31:47 +0530

    T

--- src/MainComponents/donation.js
+++ src/MainComponents/donation.js
@@ -132,7 +132,7 @@ const checker = ()=>{
             value={orderAmount}
             onChange={(e) => setOrderAmount(e.target.value)}
           ></input>
-       
+        <b>*ALL THE DONATIONS ARE TAX EXEMPTED UNDER 80G</b>
 
         <button style={{borderRadius:"10px"}} disabled={loading} onClick={checker}>
           Donate
