bfc3a650521fcd83100faaa4c1c0009c45d1ae0d
purav juneja <puravjuneja@gmail.com>
2023-04-25 19:44:20 +0530

    update

--- src/components/dailyupdates.js
+++ src/components/dailyupdates.js
@@ -1,6 +1,6 @@
 import { Fragment } from "react";
 import './dailyupdates.css';
-import pic from "../images/Picture57.jpeg";
+import pic from "../images/Picture60.jpeg";
 import pic2 from "../images/Picture59.jpeg";
 import { Link } from "react-router-dom";
 import Ticker from "./ticker";
@@ -13,8 +13,8 @@ const Dailyupdate = () =>{
         <div className = "fundRaising2">
             <div className="fundLeft2">
                 <h5>Daily Updates</h5>
-                <h1>Republic Day Event</h1>
-                <p>To celebrate the special occasion of 26th Jan SPWF had organized multiple programs. We were joined by around 120 students, our beloved chief guests and Founder Ratna ma'am along with Anil sir and all our staff. The first event was a Saraswati Puja. After prasad distribution, the Tiranga was unfurled by our dear Ratna ma'am. Many students performed onstage. The Sports Day was also conducted. Many exciting games and races were organized for the juniors as well as seniors.</p>
+                <h1>Annual Day Event</h1>
+                <p>'Sankalp’, the much awaited 5th annual day function of SPWF was held with much enthusiasm on 16th April 2023. Dr. Sangeeta Mutreja( MBBS) graced the occasion as the Chief Guest .The students enthralled the audience with their dance performances  like Ganesh vandana, fruit dance, bum bum bole, theme song , dance ka bhoot. The skit put up by them showed the remarkable impact SPWF had made on their lives. Towards the end there was the prize distribution ceremony for the students of the Ngo. A big round of applause for the students, the volunteers and also the sponsors for making the event a great success.</p>
                 <div className="playButton">
                 <a  style={{color:"white"}} href="https://youtu.be/dB33mlhTm2s" ><i class="fa-solid fa-circle-play"></i></a>
                     <p>NGO's DOCUMENTRY</p>
